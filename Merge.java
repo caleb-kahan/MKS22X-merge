04102b62731b7916589fc116cde5894f02b6ef27
Caleb Kahan <ckahan00@stuy.edu>
2019-03-18 14:44:54 -0400

    perfected incremntation

--- Merge.java
+++ Merge.java
@@ -1,13 +1,25 @@
 public static void mergesort(int[]data){
   mergesort(data,0,data.length-1);
 }
-public static void mergesort(data,lo,hi):
+public static void mergesort(data,lo,hi){
   if (lo >= hi) return;
   mergesort(data,lo,hi/2);
   mergesort(data,hi/2+1,hi);
   int [] merger = new int[hi-low+1];
-  int left =0;
-  int right =0;
-  int leftLimit = hi/2-lo + 1;
-  int rightLimit =hi-hi/2;
-  while(left>)
+  int left =lo;
+  int right =hi/2+1;
+  int leftLimit = hi/2;
+  int rightLimit =hi;
+  while(left<=leftLimit || right<=rightLimit){
+    //Just in case one gets used up
+    if (left>leftLimit) {
+      merger[left+right-lo-hi/2-1]=data[right]
+      right++;
+    }
+    if (right>rightLimit) {
+      merger[left+right-lo-hi/2-1]=data[left]
+      left++;
+    }
+    if(data[left]>)
+  }
+  }
