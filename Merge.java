ab61d3de05c5d229756b0e5cb4aec350e8624a98
Caleb Kahan <ckahan00@stuy.edu>
2019-03-19 14:38:40 -0400

    fixed index out of bound

--- Merge.java
+++ Merge.java
@@ -2,23 +2,23 @@ public class Merge{
 public static void mergesort(int[]data){
   mergesort(data,0,data.length-1);
 }
-public static void mergesort(data,lo,hi){
+public static void mergesort(int[]data,int lo,int hi){
   if (lo >= hi) return;
   mergesort(data,lo,hi/2);
   mergesort(data,hi/2+1,hi);
-  int [] merger = new int[hi-low+1];
+  int [] merger = new int[hi-lo+1];
   int left =lo;
-  int right =hi/2+1;
+  int right =(hi/2)+1;
   int leftLimit = hi/2;
   int rightLimit =hi;
-  while(left<=leftLimit || right<=rightLimit){
+  while(left<leftLimit || right<rightLimit){
     //Just in case one gets used up
     if (left>leftLimit || data[right] > data[left]) {
-      merger[left+right-lo-hi/2-1]=data[right]
+      merger[left+right-lo-hi/2-1]=data[right];
       right++;
     }
-    if (right>rightLimit || data[left] >= data[rigt]) {
-      merger[left+right-lo-hi/2-1]=data[left]
+    if (right>rightLimit || data[left] >= data[right]) {
+      merger[left+right-lo-hi/2-1]=data[left];
       left++;
     }
   }
