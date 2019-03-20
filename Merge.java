ddfa7a13019d4f92ed9e886b3f5e61a0c3a7c3d3
Caleb Kahan <ckahan00@stuy.edu>
2019-03-19 23:11:16 -0400

    insertuion sort helps a bunch. One thing that's worrying me is the duplicates

--- Merge.java
+++ Merge.java
@@ -2,37 +2,60 @@ public class Merge{
 public static void mergesort(int[]data){
   mergesort(data,0,data.length-1);
 }
-public static void mergesort(int[]data,int lo,int hi){
-  if (lo >= hi) return;
-  int avr = (lo+hi)/2;
-  mergesort(data,lo,avr);
-  mergesort(data,avr+1,hi);
-  int [] merger = new int[hi-lo+1];
-  int left =lo;
-  int right =avr+1;
-  int leftLimit = avr;
-  int rightLimit =hi;
-  while(left<=leftLimit || right<=rightLimit){
-    //Just in case one gets used up
-    if (left>leftLimit){
-      merger[left+right-lo-avr-1]=data[right];
-      right++;
-    }
-    else if (right>rightLimit){
-      merger[left+right-lo-avr-1]=data[left];
-      left++;
-    }
-    else if (data[left] <= data[right]) {
-      merger[left+right-lo-avr-1]=data[left];
-      left++;
-    }
-    else if (data[right] < data[left]) {
-      merger[left+right-lo-avr-1]=data[right];
-      right++;
+private static void insertionSort(int [] data, int start, int end){
+    for(int i =start+1;i<=end;i++){
+      if(data[i]<data[i-1]){
+        int from = i-1;
+	while(from>=start && data[from]>data[i]) from--;
+	insert(data,from+1,i);
+      }
     }
   }
-  for(int i=lo;i<=hi;i++){
-    data[i]=merger[i-lo];
+  private static void insert(int [] data, int from, int to){
+    int temp = data[from];
+    data[from] = data[to];
+    for(int j=from+1;j<=to;j++){
+      int LocTemp = data[j];
+      data[j]=temp;
+      temp = LocTemp;
+
+    }
+  }
+public static void mergesort(int[]data,int lo,int hi){
+  if (hi-lo<40) {
+    insertionSort(data,lo,hi);
+  }
+  else{
+    int avr = (lo+hi)/2;
+    mergesort(data,lo,avr);
+    mergesort(data,avr+1,hi);
+    int [] merger = new int[hi-lo+1];
+    int left =lo;
+    int right =avr+1;
+    int leftLimit = avr;
+    int rightLimit =hi;
+    while(left<=leftLimit || right<=rightLimit){
+      //Just in case one gets used up
+      if (left>leftLimit){
+        merger[left+right-lo-avr-1]=data[right];
+        right++;
+      }
+      else if (right>rightLimit){
+        merger[left+right-lo-avr-1]=data[left];
+        left++;
+      }
+      else if (data[left] <= data[right]) {
+        merger[left+right-lo-avr-1]=data[left];
+        left++;
+      }
+      else if (data[right] < data[left]) {
+        merger[left+right-lo-avr-1]=data[right];
+        right++;
+      }
+    }
+    for(int i=lo;i<=hi;i++){
+      data[i]=merger[i-lo];
+    }
   }
   }
 }
