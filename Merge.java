537b32a246e6979265c3bbb13b157658729e3f41
Caleb Kahan <ckahan00@stuy.edu>
2019-03-19 22:49:30 -0400

    the cases should be seperate. Yes, more code. But same efficientcy

--- Merge.java
+++ Merge.java
@@ -4,21 +4,30 @@ public static void mergesort(int[]data){
 }
 public static void mergesort(int[]data,int lo,int hi){
   if (lo >= hi) return;
-  mergesort(data,lo,hi/2);
-  mergesort(data,hi/2+1,hi);
+  int avr = (lo+hi)/2;
+  mergesort(data,lo,avr);
+  mergesort(data,avr+1,hi);
   int [] merger = new int[hi-lo+1];
   int left =lo;
-  int right =(hi/2)+1;
-  int leftLimit = hi/2;
+  int right =avr+1;
+  int leftLimit = avr;
   int rightLimit =hi;
-  while(left<leftLimit || right<rightLimit){
+  while(left<=leftLimit || right<=rightLimit){
     //Just in case one gets used up
-    if (left>leftLimit || data[right] > data[left]) {
-      merger[left+right-lo-hi/2-1]=data[right];
+    if (left>leftLimit){
+      merger[left+right-lo-avr-1]=data[right];
       right++;
     }
-    if (right>rightLimit || data[left] >= data[right]) {
-      merger[left+right-lo-hi/2-1]=data[left];
+    else if (right>rightLimit){
+      merger[left+right-lo-avr-1]=data[left];
+      left++;
+    }
+    else if (data[right] > data[left]) {
+      merger[left+right-lo-avr-1]=data[right];
+      right++;
+    }
+    else if (data[left] >= data[right]) {
+      merger[left+right-lo-avr-1]=data[left];
       left++;
     }
   }
